f2c01768a5b8d81ec3d7ec3aa59505bf62a8b17d
Riya Acharya <ria.acharya07@gmail.com>
2021-12-21 00:50:53 +0545

    css added

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-let firstCard = 20;
+let firstCard = 10;
 let secondCard = 4;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
-let message = "";
+let message ="";
+let messageEl = document.getElementById('message-el');
+ 
 
-    function startGame(){
-    if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂";
-    } else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳";
+function startGame() {
+  if (sum <= 20) {
+    message = 'Do you want to draw a new card?';
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack!";
     hasBlackJack = true;
-    }else {
-        message = "You're out of the game! 😭"
-        isAlive = false;
-    }
-    console.log(message);
-    }
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+}
